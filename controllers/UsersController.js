7286a6658c48ec5e97672186b80e1da9134f53d2
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-30 19:38:35 +0300

    .

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,43 +1,74 @@
-import crypto from 'crypto';
-import dbClient from '../utils/db';
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import redisClient from '../utils/redis';
+import userUtils from '../utils/user';
 
-class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+class AuthController {
+  /**
+   * Should sign-in the user by generating a new authentication token
+   *
+   * By using the header Authorization and the technique of the Basic auth
+   * (Base64 of the <email>:<password>), find the user associate to this email
+   * and with this password (reminder: we are storing the SHA1 of the password)
+   * If no user has been found, return an error Unauthorized with a status code 401
+   * Otherwise:
+   * Generate a random string (using uuidv4) as token
+   * Create a key: auth_<token>
+   * Use this key for storing in Redis (by using the redisClient create previously)
+   * the user ID for 24 hours
+   * Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" }
+   * with a status code 200
+   */
+  static async getConnect(request, response) {
+    const Authorization = request.header('Authorization') || '';
 
-    // Check if email is missing
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
-    }
+    const credentials = Authorization.split(' ')[1];
 
-    // Check if password is missing
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
+    if (!credentials) { return response.status(401).send({ error: 'Unauthorized' }); }
 
-    try {
-      // Check if user already exists in the database
-      const userExists = await dbClient.usersCollection.findOne({ email });
-      if (userExists) {
-        return res.status(400).json({ error: 'Already exist' });
-      }
+    const decodedCredentials = Buffer.from(credentials, 'base64').toString(
+      'utf-8',
+    );
 
-      // Hash the password using SHA1
-      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const [email, password] = decodedCredentials.split(':');
 
-      // Create a new user object
-      const newUser = { email, password: hashedPassword };
+    if (!email || !password) { return response.status(401).send({ error: 'Unauthorized' }); }
 
-      // Insert the new user into the collection
-      const result = await dbClient.usersCollection.insertOne(newUser);
+    const sha1Password = sha1(password);
 
-      // Respond with the new user's ID and email
-      return res.status(201).json({ id: result.insertedId, email });
-    } catch (error) {
-      console.error('Error creating user:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+    const user = await userUtils.getUser({
+      email,
+      password: sha1Password,
+    });
+
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    const hoursForExpiration = 24;
+
+    await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
+
+    return response.status(200).send({ token });
+  }
+
+  /**
+   * Should sign-out the user based on the token
+   *
+   * Retrieve the user based on the token:
+   * If not found, return an error Unauthorized with a status code 401
+   * Otherwise, delete the token in Redis and return nothing with a
+   * status code 204
+   */
+  static async getDisconnect(request, response) {
+    const { userId, key } = await userUtils.getUserIdAndKey(request);
+
+    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
+
+    await redisClient.del(key);
+
+    return response.status(204).send();
   }
 }
 
-export default UsersController;
+export default AuthController;
